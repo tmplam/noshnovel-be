56ea690f8d4a2e4ddcc0185695f567fecc1dacfd
21120480 <tndkhiemqn2k3@gmail.com>
2024-04-30 12:59:35 +0700

    Implement get by keyword for tangthuvien.vn

--- NoshNovel.Servers.TangThuVien/TangThuVienCrawler.cs
+++ NoshNovel.Servers.TangThuVien/TangThuVienCrawler.cs
@@ -96,14 +96,14 @@ namespace NoshNovel.Servers.TangThuVien
 
                                             HtmlNode novelNode = novelNodes[j];
 
-                                            novelItem.Title = novelNode.SelectSingleNode("./div[@class='book-mid-info']/h4/a").InnerText;
+                                            novelItem.Title = novelNode.SelectSingleNode("./div[@class='book-mid-info']/h4/a").InnerText.Trim();
                                             novelItem.Author = novelItem.Author = new Author()
                                             {
-                                                Name = novelNode.SelectSingleNode("./div[@class='book-mid-info']/p[@class='author']/a[@class='name']").InnerText,
+                                                Name = novelNode.SelectSingleNode("./div[@class='book-mid-info']/p[@class='author']/a[@class='name']").InnerText.Trim(),
                                             };
-                                            novelItem.CoverImage = novelNode.SelectSingleNode("./div[@class='book-img-box']/a/img").GetAttributeValue("src", "");
-                                            novelItem.NovelSlug = novelNode.SelectSingleNode("./div[@class='book-mid-info']/h4/a").GetAttributeValue("href", "").Split("/", StringSplitOptions.RemoveEmptyEntries)[3];
-                                            novelItem.Status = novelNode.SelectSingleNode("./div[@class='book-mid-info']/p[@class='author']/span").InnerText;
+                                            novelItem.CoverImage = novelNode.SelectSingleNode("./div[@class='book-img-box']/a/img").GetAttributeValue("src", "").Trim();
+                                            novelItem.NovelSlug = novelNode.SelectSingleNode("./div[@class='book-mid-info']/h4/a").GetAttributeValue("href", "").Split("/", StringSplitOptions.RemoveEmptyEntries)[3].Trim();
+                                            novelItem.Status = novelNode.SelectSingleNode("./div[@class='book-mid-info']/p[@class='author']/span").InnerText.Trim();
                                             novelItem.TotalChapter = int.Parse(novelNode.SelectNodes("./div[@class='book-mid-info']/p[@class='author']/span")[1].SelectSingleNode("./span").InnerText);
 
                                             novelItems.Add(novelItem);
@@ -160,7 +160,153 @@ namespace NoshNovel.Servers.TangThuVien
 
         public NovelSearchResult GetByKeyword(string keyword, int page = 1, int perPage = 18)
         {
-            throw new NotImplementedException();
+            // Calculate page and position to crawl
+            int startPosition = (page - 1) * perPage + 1;
+            int firstCrawledPage = startPosition / maxPerCrawlPage + 1;
+            int crawlPosition = startPosition % maxPerCrawlPage - 1;
+
+            keyword = string.Join("%20", keyword.Split(" ", StringSplitOptions.RemoveEmptyEntries));
+            var url = $"{baseUrl}/ket-qua-tim-kiem?term={keyword}";
+
+            NovelSearchResult searchResult = new NovelSearchResult();
+            searchResult.Page = page;
+            searchResult.PerPage = perPage;
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                try
+                {
+                    // make first request
+                    HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url);
+                    HttpResponseMessage response = httpClient.Send(request);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string responseContent = response.Content.ReadAsStringAsync().Result;
+                        // Decodes html-encoded
+                        responseContent = System.Net.WebUtility.HtmlDecode(responseContent);
+                        HtmlDocument doc = new HtmlDocument();
+                        doc.LoadHtml(responseContent);
+
+                        // Total crawl pages
+                        int totalCrawlPages = 1;
+
+                        HtmlNodeCollection paginationElementNodes = doc.DocumentNode.SelectNodes("//ul[@class='pagination']/li/a");
+
+                        // In case more than one page returned
+                        if (paginationElementNodes != null)
+                        {
+                            HtmlNode lastNumNode = paginationElementNodes[paginationElementNodes.Count - 2];
+                            totalCrawlPages = int.Parse(lastNumNode.InnerText);
+                        }
+
+                        int novelCountDown = perPage;
+                        List<NovelItem> novelItems = new List<NovelItem>();
+                        // Crawl novel items
+                        for (int i = firstCrawledPage; i <= totalCrawlPages && novelCountDown > 0; i++)
+                        {
+                            // If the first crawled page equals one, no need to make new request
+                            if (firstCrawledPage > 1)
+                            {
+                                // Make more requests
+                                url = $"{baseUrl}/ket-qua-tim-kiem?term={keyword}&page={i}";
+                                request = new HttpRequestMessage(HttpMethod.Get, url);
+                                response = httpClient.Send(request);
+
+                                if (response.IsSuccessStatusCode)
+                                {
+                                    responseContent = response.Content.ReadAsStringAsync().Result;
+                                    // Decodes html-encoded
+                                    responseContent = System.Net.WebUtility.HtmlDecode(responseContent);
+                                    doc.LoadHtml(responseContent);
+                                }
+                            }
+
+                            HtmlNodeCollection novelNodes = doc.DocumentNode.SelectNodes("//div[@class='main-content-wrap fl']/div[@class='rank-body']/div/div/ul/li");
+
+                            // Not found novels
+                            if (novelNodes == null || novelNodes[0].SelectSingleNode("./div") == null)
+                            {
+                                break;
+                            }
+
+                            for (int j = crawlPosition; j < novelNodes.Count; j++)
+                            {
+                                NovelItem novelItem = new NovelItem();
+
+                                HtmlNode novelNode = novelNodes[j];
+
+                                novelItem.Title = novelNode.SelectSingleNode("./div[@class='book-mid-info']/h4/a").InnerText.Trim();
+                                novelItem.Author = novelItem.Author = new Author()
+                                {
+                                    Name = novelNode.SelectSingleNode("./div[@class='book-mid-info']/p[@class='author']/a[@class='name']").InnerText.Trim(),
+                                };
+                                novelItem.CoverImage = novelNode.SelectSingleNode("./div[@class='book-img-box']/a/img").GetAttributeValue("src", "").Trim();
+                                // Get novel slug
+                                HtmlNode novelLinkNode = novelNode.SelectSingleNode("./div[@class='book-mid-info']/h4/a");
+                                var novelLinkHtmlParts = novelLinkNode.OuterHtml.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                                if (novelLinkHtmlParts[1][^2] == '/')
+                                {
+                                    string splitedString = novelLinkHtmlParts[2].Split("=")[0];
+                                    splitedString = splitedString.Remove(splitedString.Length - 1);
+                                    novelItem.NovelSlug = $"\"{splitedString}";
+                                }
+                                else
+                                {
+                                    novelItem.NovelSlug = novelLinkNode.GetAttributeValue("href", "").Split("/", StringSplitOptions.RemoveEmptyEntries)[3].Trim();
+                                }
+
+                                novelItem.Status = novelNode.SelectSingleNode("./div[@class='book-mid-info']/p[@class='author']/span").InnerText.Trim();
+                                novelItem.TotalChapter = int.Parse(novelNode.SelectNodes("./div[@class='book-mid-info']/p[@class='author']/span")[1].SelectSingleNode("./span").InnerText);
+
+                                novelItems.Add(novelItem);
+
+                                if (--novelCountDown == 0)
+                                {
+                                    break;
+                                }
+                            }
+
+                            crawlPosition = 0;
+                        }
+
+                        searchResult.Data = novelItems;
+
+                        url = $"{baseUrl}/ket-qua-tim-kiem?term={keyword}&page={totalCrawlPages}";
+                        // Calculate total novels
+                        request = new HttpRequestMessage(HttpMethod.Get, url);
+                        response = httpClient.Send(request);
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            responseContent = response.Content.ReadAsStringAsync().Result;
+                            // Decodes html-encoded
+                            responseContent = System.Net.WebUtility.HtmlDecode(responseContent);
+                            doc.LoadHtml(responseContent);
+
+                            int totalNovels = 0;
+
+                            HtmlNodeCollection lastPageNodes = doc.DocumentNode.SelectNodes("//div[@class='main-content-wrap fl']/div[@class='rank-body']/div/div/ul/li");
+
+                            if (lastPageNodes != null && lastPageNodes[0].SelectSingleNode("./div") != null)
+                            {
+                                int novelOflastCrawedPage = lastPageNodes.Count;
+                                totalNovels = (totalCrawlPages - 1) * maxPerCrawlPage + novelOflastCrawedPage;
+                            }
+
+                            searchResult.Total = totalNovels;
+                            searchResult.TotalPages = totalNovels / perPage + (totalNovels % perPage == 0 ? 0 : 1);
+                        }
+                    }
+                }
+                catch (HttpRequestException ex)
+                {
+                    Console.WriteLine($"An error occurred: {ex.Message}");
+                }
+            }
+
+            return searchResult;
         }
 
         public NovelChaptersResult GetChapterList(string novelSlug, int page = 1, int perPage = 40)
